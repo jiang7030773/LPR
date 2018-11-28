40a52892a6fbb8ced8c4256203184e6330ac9b34
jiangyu(蒋宇) <jiangyu@aee.ad>
2018-11-28 16:32:29 +0800

    图片修改
    删掉了重复的图片、统一命名图片

--- car_pic/rename_jpg.py
+++ car_pic/rename_jpg.py
@@ -1,8 +1,19 @@
+'''写了点小代码，统一图片命名和格式,删掉有重复命名的'''
+
 import os
-import cv2
+
+def rename(filepath):
+    renames_nums = 0
+    images = os.listdir(filepath)
+    for name in images:
+        if len(name) != 11:
+            os.rename(filepath+name,filepath+name[0:7]+'.jpg')
+            renames_nums += 1
+    print("修改了%d张图片名"%renames_nums)
 
 
-def ranamesJPG(filepath):
+
+def Modify_suffix(filepath):
     renames_nums = 0
     images = os.listdir(filepath)
     for name in images:
@@ -11,8 +22,21 @@ def ranamesJPG(filepath):
             os.rename(filepath+name, filepath+'浙A'+name.split('.')[0]+'.jpg')
             print("修改成功第%d张"%renames_nums)
             continue
-         
+
+def del_Repeat_name(filepath):
+    Repeat_name = 0
+    list1 = []
+    images = os.listdir(filepath)
+    for name in images:
+        if name[0:7] not in list1:
+            list1.append(name[0:7])
+        else:
+            os.remove(filepath+name)
+            Repeat_name += 1
+    print('删掉了%d张'%Repeat_name)   
 
 if __name__ == '__main__':
-    filepath = "./bmp/"
-    ranamesJPG(filepath)
+    filepath = "./car_pic/image/train/"
+    rename(filepath)
+    del_Repeat_name(filepath)
+    rename(filepath)
--- hyperlpr-train/test.py
+++ hyperlpr-train/test.py
@@ -5,5 +5,5 @@ import numpy as np
 img_dir = './car_pic/image/train/'
 filename = '川010168'
 # img = cv2.imread(os.path.join(img_dir, '川S77777.jpg'))
-img = cv2.imdecode(np.fromfile(img_dir + '川S77777.jpg', dtype=np.uint8), -1)
+img = cv2.imdecode(np.fromfile(img_dir + '川S77777\n'.strip()+'.jpg', dtype=np.uint8), -1)
 print(img)
